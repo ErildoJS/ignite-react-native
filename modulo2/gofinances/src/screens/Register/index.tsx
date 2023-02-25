f2a5cb9a008a7e4f41c70cccb32690892643c7f4
ErildoJS <patriciopatrick92@gmail.com>
2023-02-25 13:48:44 +0100

    create handleRegister function

--- modulo2/gofinances/src/screens/Register/index.tsx
+++ modulo2/gofinances/src/screens/Register/index.tsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { Modal, Text, View } from 'react-native';
 import { Button } from '../../components/form/Button';
 import { CategorySelectButton } from '../../components/form/CategorySelectButton';
 import { Input } from '../../components/form/Input';
+import { InputForm } from '../../components/form/InputForm';
 import { TransactionTypeButton } from '../../components/form/TransactionTypeButton';
 import { CategorySelect } from '../CategorySelect';
 import { styles } from './styles';
 
+interface formData {
+  name: string;
+  amount: number;
+}
 
 export function Register() {
   const [transactionType, setTransactionType] = useState('')
@@ -16,6 +22,7 @@ export function Register() {
     name: 'Categoria',
   })
 
+  const {control, handleSubmit} = useForm()
 
   function handleTransactionsTypeSelect(type: 'up' | 'down') {
     setTransactionType(type)
@@ -29,6 +36,18 @@ export function Register() {
     setCategoryModalOpen(false)
   }
 
+  function handleRegister(form: formData) {
+    const data = {
+      name: form.name,
+      amount: form.amount,
+      transactionType,
+      category: category.key
+    }
+
+    console.log(data);
+    
+  }
+
   return (
     <View style={styles.Container}>
       <View style={styles.header}>
@@ -37,8 +56,8 @@ export function Register() {
 
       <View style={styles.form}>
         <View style={styles.fields}>
-          <Input placeholder='Nome'/>
-          <Input placeholder='Preco'/>
+          <InputForm name='name' control={control} placeholder='Nome'/>
+          <InputForm name='amount' control={control}placeholder='Preco'/>
           <View style={styles.transactionsTypes}>
             <TransactionTypeButton title='Income' type='up' onPress={() => handleTransactionsTypeSelect('up')} isActive={transactionType === 'up'}/>
             <TransactionTypeButton title='Outcome' type='down' onPress={() => handleTransactionsTypeSelect('down')} isActive={transactionType === 'down'}/>
@@ -46,7 +65,7 @@ export function Register() {
 
           <CategorySelectButton title={category.name} onPress={handleOpenSelectCategoryModal}/>
         </View>
-        <Button title='Enviar'/>
+        <Button title='Enviar' onPress={handleSubmit(handleRegister)}/>
       </View>
 
       <Modal visible={categoryModalOpen}>
